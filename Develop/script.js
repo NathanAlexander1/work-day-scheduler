c730c81e192f055b7ad62d9152e7242359ba7d39
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-10-05 23:00:43 -0700

    created variables in JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,29 @@
-//creat jumbotron header
-    //include an h3
-    //include timedisplay in h3
+var currentDay = document.querySelector("#currentDay");
 
-//
+var theTime = moment().format("dddd, MMMM Do");
+currentDay.textContent = theTime;
+
+
+var discriptionBox = $(".description");
+var saveBtn = $('.saveBtn');
+
+
+// var blockOn
+
+//create fillable forms
+//if a task takes place in the past, turn itpink
+//if a task takes place in the present, turn it red
+//if a task takes place in the future, turn it green
+
+//when i click the save button, the text from the fillable form is saved to local storage
+
+
+
+
+//for loop
+//if current hour is less than current time
+    //turn pink
+//if current hour is equal to time
+    //turn green
+//if current hour greater than current time
+    //turn orange
