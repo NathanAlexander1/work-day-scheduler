c227272ba50911463dbb4c07764341a5c640930e
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-10-06 08:45:23 -0700

    text area saving to local storage but not saving upon refresh

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,28 @@
 var currentDay = document.querySelector("#currentDay");
-
 var theTime = moment().format("dddd, MMMM Do");
 currentDay.textContent = theTime;
-
-
 var discriptionBox = $(".description");
 var saveBtn = $('.saveBtn');
 var hour = $('.hour');
-
 var currentHour = moment().hour();
-console.log(currentHour);
+
+// variables grabbing buttons
+var btnNine = $('#btnNine');
+
+// variables grabbing text areas
 
 
-var timesObject = {nine: $("#9"), ten: $("#10"), eleven:$("#11"), twelve: $("#12"), thirteen: $("#13"), fourteen: $("#14"), fifteen: $("#15"), sixteen: $("#16"), seventee: $("#17")};
+
+
+var timesArrofObj = [{nine: $("#9")}, {ten: $("#10")}, {eleven: $("#11")}, {twelve: $("#12")}, {thirteen: $("#13")}, {fourteen: $("#14")}, {fifteen: $("#15")}, {sixteen: $("#16")}, {seventeen: $("#17")}];
+
+console.log(timesArrofObj[4]);
+//on click: fucntion{}
+//iterate over array of objects
+//test:console log each object
+//if array at i is not an empty string
+//save to local storage
+//retrieve from local storage and give text content of arr[i].key to description
 
 function changeColor() {
     for (var i = 9; i < 18; i++) {
@@ -27,6 +37,16 @@ function changeColor() {
 }
 
 changeColor();
+
+btnNine.on('click', function () {
+    var nineText = $("#9").val();
+    localStorage.setItem("textareanine", JSON.stringify(nineText));
+
+    var savedTextAreaContent = JSON.parse(localStorage.getItem("textareanine"));
+
+    nineText = savedTextAreaContent;
+
+  });
 //create fillable forms
 //if a task takes place in the past, turn itpink
 //if a task takes place in the present, turn it red
